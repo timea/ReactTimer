af869ad7a01fce068010f4569d817e211ef59597
Timea Kiss <k.timea.k@gmail.com>
2016-10-08 08:50:41 +0200

    Add Timer and Countdown elements

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = () => {
+  return (
+    <div>
+      Countdown.jsx Rendered
+    </div>
+  );
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = () => {
+  return (
+    <div>
+      Timer.jsx Rendered
+    </div>
+  );
+}
+
+module.exports = Timer
