b6b20d5e1bc7dc11243f3d7b276875ebbdea24af
Timea Kiss <k.timea.k@gmail.com>
2016-10-08 09:00:24 +0200

    Move inline CSS to styles/components

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -11,10 +11,10 @@ var Nav = React.createClass({
                 React Time App
             </li>
             <li>
-              <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+              <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
             </li>
             <li>
-              <Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+              <Link to="/countdown" activeClassName="active-link">Countdown</Link>
             </li>
           </ul>
         </div>
