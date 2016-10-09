0f4fc9df7ebe63b979cdec74137e9bc6e8858a59
Timea Kiss <k.timea.k@gmail.com>
2016-10-09 23:11:33 +0200

    Add CountdownForm component and tests

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+
 var Clock = require('Clock');
 
 describe('Clock', () => {
