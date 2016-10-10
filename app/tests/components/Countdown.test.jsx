1c93d3869373dde5ca616a8da32c9407e7d37c99
Timea Kiss <k.timea.k@gmail.com>
2016-10-10 06:14:37 +0200

    Finalize tests & add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -22,7 +22,7 @@ describe('Countdown', () => {
       setTimeout(() => {
         expect(countdown.state.count).toBe(9);
         done();
-      },1001)
+      }, 1001);
     });
 
     it('should stop countdown at 0', (done) => {
@@ -32,7 +32,31 @@ describe('Countdown', () => {
       setTimeout(() => {
         expect(countdown.state.count).toBe(0);
         done();
-      },3001)
+      }, 3001);
+    });
+
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should stop countdown on stopped status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
   });
 });
